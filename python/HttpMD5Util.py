ab56861962ea8033bd9521083dd7f3af7d99ed04
franklee00 <lizehangsz@gmail.com>
2016-05-12 14:41:30 +0800

    Update HttpMD5Util.py
    对httplib在Python2和Python3的不同写法进行备注，并附上相应的修改后的代码，用户可以根据使用的是Python2或Python3进行选择。

--- python/HttpMD5Util.py
+++ python/HttpMD5Util.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #用于进行http请求，以及MD5加密，生成签名的工具类
 
-import http.client
+import http.client#本行是Python3写法，如您使用Python3则无问题。如您使用Python2.7，则应修改为 import httplib .以下语句还有3出修改，分别在第20行，第31行和第33行
 import urllib
 import json
 import hashlib
@@ -16,6 +16,7 @@ def buildMySign(params,secretKey):
     return  hashlib.md5(data.encode("utf8")).hexdigest().upper()
 
 def httpGet(url,resource,params=''):
+    #以下行为Python3写法，如使用Python2.7，则改为 conn = httplib.HTTPSConnection(url, timeout=10)
     conn = http.client.HTTPSConnection(url, timeout=10)
     conn.request("GET",resource + '?' + params)
     response = conn.getresponse()
@@ -26,7 +27,9 @@ def httpPost(url,resource,params):
      headers = {
             "Content-type" : "application/x-www-form-urlencoded",
      }
+     #以下行为Python3写法，如使用Python2.7，则改为 conn = httplib.HTTPSConnection(url, timeout=10)
      conn = http.client.HTTPSConnection(url, timeout=10)
+     #以下行为Python3写法，如使用Python2.7，则改为 temp_params = urllib.urlencode(params)
      temp_params = urllib.parse.urlencode(params)
      conn.request("POST", resource, temp_params, headers)
      response = conn.getresponse()
